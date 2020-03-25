c333c232b416f4207b4cd49db58371a0caee5cf2
Anders Mannberg <anders.mannberg@softhouse.se>
2020-03-25 11:38:51 +0100

    Fixed typo

--- Tests/AppTests/PasswordTests.swift
+++ Tests/AppTests/PasswordTests.swift
@@ -8,16 +8,18 @@ final class PasswordTests: XCTestCase {
     
     func testPassword_shouldContainAtLeastOneDigit() {
         XCTAssert(Validator.password.validate("aaaaaaaa").isFailure)
+        
+        XCTAssertFalse(Validator.password.validate("ABCDEFG1").isFailure)
     }
     
-    func testPassword_shouldContainAtLeastCharacter() {
-        XCTAssertFalse(Validator.password.validate("ABCDEFG1").isFailure)
+    func testPassword_shouldContainAtLeastOneCharacter() {
         XCTAssert(Validator.password.validate("11111111").isFailure)
         XCTAssert(Validator.password.validate("1#######").isFailure)
     }
     
     func testPassword_shouldContainOnlyEnglishLettersNumbersAndSomeSpecialCharacters() {
         XCTAssert(Validator.password.validate("aaaaaaå1").isFailure)
+        
         XCTAssertFalse(Validator.password.validate("aaaaaaa1").isFailure)
         XCTAssertFalse(Validator.password.validate("aaaaaa1_").isFailure)
         XCTAssertFalse(Validator.password.validate("aaaaaa1!").isFailure)
